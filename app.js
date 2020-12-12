5dec78cdef2f955c7033510e12f48748e397b452
akpmohan <mohankumar.m@examly.in>
2020-12-12 23:40:11 +0530

    done Creating and validation

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-import {db} from './object-db.mjs'
+import {db} from './object-db.mjs';
+
 let dataStore = new db()
-const promise = new Promise((resolve,reject)=>{
-    dataStore.start(process.argv[2])
-    // resolve(true)
-});
-promise.then(dataStore.insert())
-console.debug(dataStore.data)
+// const promise = new Promise(async(resolve,reject)=>{
+//    await dataStore.start(process.argv[2])
+//    .then((resp) => {
+//        console.log(resp, 'ressp--->')
+//        if (resp) {
+//         promise.then(dataStore.insert())
+//         console.log(dataStore, 'datastore')
+//        }
+//    })
+//    .catch((error)=>{
+//        console.log(error)
+//    })
+//     resolve(true)
+// });
+dataStore.start(process.argv[2])
+   .then((resp) => {
+       dataStore.create('asf',{"af":{'dfsdf':56}})
+       .then((res)=>{
+        console.log(res)
+       })
+       .catch((error)=>{
+           console.log(error)
+       })
+   })
+   .catch((error)=>{
+       console.log(error)
+   })
--- object-db.mjs
+++ object-db.mjs
@@ -4,6 +4,7 @@ export class db{
     constructor(){
     }
     async start(path){
+        return new Promise(async(resolved, rejected) => {
         if(path){
             console.log('\n---Instantiating DB instance in given Path---\n')
             }
@@ -14,47 +15,100 @@ export class db{
         try{
             if(!fs.existsSync(path)){
                 throw('!!!Filepath does not exists.Please give Absolute path!!!\n')
+                
             }
-            this.twirlTimer(1000);
             console.log('---Checking for previous DB---\n')
             try{
-                this.twirlTimer(1000);
                 this.data = {}
                 if(!fs.existsSync(path+'/db.json')){
-                    fs.writeFile(path+'/db.json',JSON.stringify(this.data),function(err){
-                        if(null){
-                        throw(err)
+                    await fs.writeFile(path+'/db.json',JSON.stringify(this.data),function(err){
+                        if (err) {
+                            console.log(err)
+                            rejected(err)
+                        }
+                        else{
+                            console.log(`---DB doest not exists---\n---creating new one @ ${path+'db.json'}---\n`)
+                            this.dbPath = path+'db.json'
+                            resolved(true)
                         }
                     })
-                    throw(`---DB doest not exists---\n---creating new one @ ${path+'/db.json'}---\n`)
+                } else {
+                    await fs.readFile(path + '/db.json', 'utf8', (error, fileData) => {
+                        if (error) {
+                            rejected(error);
+                        } else {
+                            this.data = JSON.parse(fileData)
+                            this.dbPath = path+'db.json'
+                            console.log(`-----Existing DB detected @ ${path+'db.json'}\n`)
+                            console.log('-----Opening existing DB-----\n')
+                            resolved(true);
+                        }
+                    })
                 }
-                fs.readFile(path+'/db.json','utf8',(error,fileData)=>{
-                    if(error)
-                    console.log(error)
-                    this.data = JSON.parse(fileData)
-                    console.log(this.data)
-                })
-                console.log(`-----Existing DB detected @ ${path+'/db.json'}\n`)
-                console.log('-----Opening existing DB-----\n')
             }
-            catch(message){
-                console.log(message)
+            catch(error){
+                console.log(error)
             }
 
         }catch(error){
-            console.log(error)
+            rejected(error)
         }
+    });
     }
-    async insert(key,value){
-        console.log(this.data)
-        console.log('insert')
-    }
-   twirlTimer(time) {
-        const interval = setInterval(function() {
-            process.stdout.write("#")
-        }, 100);
-        setTimeout(function(){
-            clearInterval(interval)
-        },time)
-      }
+    async create(key,value){
+            return new Promise(async(resolved, rejected) => {
+                const rejectReason = []
+                console.log(Buffer.byteLength(JSON.stringify(this.data)))
+                if(Buffer.byteLength(JSON.stringify(this.data))/1024 > 1024){
+                    rejectReason.push(`Datastore size limit exceding 1GB. Cannot create.`)
+                    rejected(rejectReason)
+                    return
+                }
+                if(key == null){
+                    rejectReason.push('Key should not be null')
+                }
+                if(typeof key != 'string'){
+                    rejectReason.push('The type of key should be string')
+                }
+                if(typeof value != 'object'){
+                    rejectReason.push('The type of value should be object')
+                }
+                else if(value == null || Object.keys(value).length === 0){
+                    rejectReason.push('Value should not be null or Empty')
+                }
+                if(key.length<=0 || 32<key.length){
+                    rejectReason.push('The key should have Max of 32 chars and Min of 1 char.')
+                }
+                if(Object.keys(this.data).includes(key)){
+                    rejectReason.push('Key already exists.Key should be unique.')
+                }
+                if(Buffer.byteLength(JSON.stringify(value))/1024 > 16){
+                    rejectReason.push(`Value size should not be greate than 16KB.Your value size ${(Buffer.byteLength(JSON.stringify(value))/1024).toFixed(2)}KB`);
+                }
+
+                if(rejectReason.length){
+                    rejected(rejectReason)
+                    return
+                }
+                else{
+                    this.data[key] = value;
+                    let copy = this.data
+                    console.log(this.data)
+                    await fs.writeFile(this.dbPath,JSON.stringify(copy),function(err){
+                        if (err) {
+                            console.log(err)
+                            rejected(err)
+                        }
+                        else{
+                            console.log(this.data)
+                            let a = `Space Availabe:${(Buffer.byteLength(JSON.stringify(this.data))/1024/1024)}MB/1024MB`
+                            resolved(a)
+                        }
+                    })
+                    console.log(this.data)
+                }
+
+        }
+    );
+}
 }
